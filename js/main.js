7b3ee10bbf388ea9d424565c72d3965acaf3d927
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-10-15 17:28:11 +0700

    feat: add project

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ robot.style.left = 0;
 const robotRun = () => {
     const runRight = () => {
         newLeft += 2;
-        if (newLeft > screen.width-robot.width) {
+        if (newLeft > screen.width - robot.width) {
             defaultRun = false;
             robot.style.transform = 'scale(-1, 1)';
         }
@@ -31,7 +31,7 @@ const robotRun = () => {
     } else {
         runLeft();
     }
-    robot.style.left = `${newLeft}px`;    
+    robot.style.left = `${newLeft}px`;
     animation = window.requestAnimationFrame(robotRun);
 };
 
@@ -39,11 +39,11 @@ mainBtn.addEventListener('click', (event) => {
     let target = event.target;
     if (target.classList.contains('start')) {
         robotRun();
-        robot.src = '../img/robot.gif';
+        robot.src = 'img/robot.gif';
     }
     if (target.classList.contains('stop')) {
         cancelAnimationFrame(animation);
-        robot.src = '../img/robot.jpg';
+        robot.src = 'img/robot.jpg';
     }
     mainBtn.classList.toggle('start');
     mainBtn.classList.toggle('stop');
@@ -52,7 +52,7 @@ mainBtn.addEventListener('click', (event) => {
 
 reset.addEventListener('click', () => {
     if (mainBtn.classList.contains('start')) {
-        robot.src = '../img/robot.gif';
+        robot.src = 'img/robot.gif';
     }
 
     defaultRun = true;
